64525f297286e53fadcca2ea674b7d8e325b4dcd
Jakub Nowak <jakub.nowak.eu@gmail.com>
2022-10-08 22:25:59 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'portfolio';
+
+  ngOnDestroy(): void {
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleRefresh(): void {
+
+  }
+
+  reset(): void {
+
+  }
+
+  private load(): void {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
 import { AppComponent } from './app.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { AlertComponent } from './alert/alert.component';
+import { TickerComponent } from './ticker/ticker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestmentsComponent,
+    StocksComponent,
+    AlertComponent,
+    TickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
